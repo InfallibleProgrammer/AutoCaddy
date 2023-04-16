78fd5b49d524431ee8b2d9539e8fbe42a5a4c6a3
Justin Stokes <JustinStokes@stokes.j1215@gmail.com>
2023-04-15 22:51:26 -0700

    removes comment

--- projects/lpc40xx_freertos/l5_application/ble_position/ble_position.h
+++ projects/lpc40xx_freertos/l5_application/ble_position/ble_position.h
@@ -16,20 +16,5 @@ typedef struct {
   long double longitutde;
 } coordinate_s;
 
-//  if (uart__get(UART__3, &message_buffer[index], 1000U)) {
-//       if (message_buffer[index] == ':') {
-//         // parse keyword
-//         keyword_e keyword = parse_keyword(message_buffer, index);
-//         memset(message_buffer, 0, sizeof(message_buffer));
-//         index = 0U;
-//       } else if (message_buffer[index] == ',') {
-//         // parse lat/ long
-//         const long double value = parse_latitude_longitude(message_buffer, index);
-//         memset(message_buffer, 0, sizeof(message_buffer));
-//         index = 0U;
-//       } else {
-//         index++;
-//       }
-
 void ble_position__init(void);
 bool ble_position__periodic(coordinate_s *cooridnate);
